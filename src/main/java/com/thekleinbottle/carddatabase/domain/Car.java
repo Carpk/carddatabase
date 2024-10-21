1cafc625adf53d05df8c6ba11649ba7b7d2931f9
Invalid <zappeln@gmail.com>
2024-10-21 07:48:29 -0500

    add owner class with relations

--- src/main/java/com/thekleinbottle/carddatabase/domain/Car.java
+++ src/main/java/com/thekleinbottle/carddatabase/domain/Car.java
@@ -2,9 +2,12 @@ package com.thekleinbottle.carddatabase.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Car {
@@ -13,9 +16,21 @@ public class Car {
     private Long id;
     private String brand, model, color, registrationNumber;
     private int modelYear, price;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="owner")
+    private Owner owner;
     
     // @Column(name="explanation", nullable=false, length=512)
     // private String description;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
 
     public Car() {
     }
--- src/main/java/com/thekleinbottle/carddatabase/domain/CarRepository.java
+++ src/main/java/com/thekleinbottle/carddatabase/domain/CarRepository.java
@@ -3,10 +3,12 @@ package com.thekleinbottle.carddatabase.domain;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CarRepository extends CrudRepository<Car,Long> {
+public interface CarRepository extends CrudRepository<Car, Long> {
     List<Car> findByBrand(String brand);
     
     List<Car> findByColor(String color);
     
     List<Car> findByModelYear(int modelYear);
+
+    // 47
 }
