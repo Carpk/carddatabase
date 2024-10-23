1c5ce4a7d263ef6961f69ca58c5dcfcb228067d9
Invalid <zappeln@gmail.com>
2024-10-23 11:54:05 -0500

    swagger and OpenAPI

--- src/main/java/com/thekleinbottle/carddatabase/domain/CarRepository.java
+++ src/main/java/com/thekleinbottle/carddatabase/domain/CarRepository.java
@@ -1,14 +1,17 @@
 package com.thekleinbottle.carddatabase.domain;
 
 import java.util.List;
-import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
-    List<Car> findByBrand(String brand);
+    List<Car> findByBrand(@Param("brand") String brand);
     
-    List<Car> findByColor(String color);
+    List<Car> findByColor(@Param("color") String color);
     
     List<Car> findByModelYear(int modelYear);
 
-    // 47
 }
