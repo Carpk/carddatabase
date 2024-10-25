625c06f62d1d61bc614ec902c58c9a22b9163c04
Invalid <zappeln@gmail.com>
2024-10-24 20:39:20 -0500

    successful login with db user

--- src/main/java/com/thekleinbottle/carddatabase/SecurityConfig.java
+++ src/main/java/com/thekleinbottle/carddatabase/SecurityConfig.java
@@ -2,14 +2,18 @@ package com.thekleinbottle.carddatabase;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.thekleinbottle.carddatabase.service.UserDetailsServiceImpl;
 
@@ -25,17 +29,25 @@ public class SecurityConfig {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
-    
-    // @Bean
-    // public InMemoryUserDetailsManager userDetailsService() {
-    //     UserDetails user = User.builder().username("user").
-    //         password(passwordEncoder().encode("password")).roles("USER").build();
-        
-    //     return new InMemoryUserDetailsManager(user);
-    // }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager uthenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+            .sessionManagement((sessionManagement) -> 
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((authorizeHttpRequests) -> 
+                authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated());
+
+        return http.build();
     }
 }
